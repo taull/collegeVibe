6b4b4febce96f11e071f251206d28a7e73d2921a
Taylor Aull <aull.taylor@gmail.com>
2015-05-11 18:07:25 -0400

    started to add slide out sidebar, doesn't quite work yet

---
+++ app/controllers/application.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+
+	openSidebar: function(){
+        Ember.$('.home-sidebar').addClass('open-sidebar');
+
+        Ember.$('.home-sidebar-open').addClass('hidden');
+        Ember.$('.home-sidebar-close').removeClass('hidden');
+    },
+
+    closeSidebar: function(){
+        Ember.$('.home-sidebar').removeClass('open-sidebar');
+
+        Ember.$('.home-sidebar-close').addClass('hidden');
+        Ember.$('.home-sidebar-open').removeClass('hidden');
+    },
+});
