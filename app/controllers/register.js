34678bb77b8ff8b60aa16c20c2a6250f003818ae
jhath16 <hathaway.justin@gmail.com>
2015-05-14 21:54:55 -0400

    Changes the user posting to only submit name and email

--- app/controllers/register.js
+++ app/controllers/register.js
@@ -6,21 +6,24 @@ export default Ember.Controller.extend({
 
 	actions: {
 		register: function () {
+			//Get the values from the text boxes
 			var name = this.get('name');
 			var email = this.get('email');
 			var password = this.get('password');
-
+			//Create the constructor for the user object
 			var users = Parse.Object.extend({
 				className: 'user',
-
+				//Set the attributes on the instanced object
 				initialize: function () {
 					this.set({name:name});
 					this.set({email:email});
-					this.set({password:password});
 				}
 			});
+			//Create an instance of the user-class object and save it to the server
 			var newUser = new users().save();
-			console.log(newUser);
+			//Change to the index route once registered
+			//(Might want to make this run after a successful callback)
+			this.transitionToRoute('index');
 	}
 }
 
