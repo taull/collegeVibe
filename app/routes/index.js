d42b159ef006fe555eeaa9698240af962f7a07e3
jhath16 <hathaway.justin@gmail.com>
2015-06-05 21:48:29 -0400

    Loads college class from Parse to model of indexRoute

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  afterModel: function () {
+      console.log(this.controllerFor);
+  },
+
+  model: function () {
+    var query = new Parse.Query("College");
+    return query.find({
+      success:function (results) {
+        return results;
+      },
+      error:function (object, error) {
+        alert(error);
+      }
+    });
+  }
+
+
+
 });
